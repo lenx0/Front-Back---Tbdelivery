5705359ad6b02d9e14bdf23d12a26bd4a05a9cd9
Thiago <thiagofreitas1106@gmail.com>
2021-01-08 20:28:25 -0300

    Project created

---
+++ backend/src/main/java/com/devthiago/tbdelivery/TbdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devthiago.tbdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devthiago/tbdelivery/TbdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devthiago.tbdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
