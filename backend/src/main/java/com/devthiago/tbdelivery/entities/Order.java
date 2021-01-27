5ce765439e585761342ba305fdc4770d78cfdfa9
Thiago <thiagofreitas1106@gmail.com>
2021-01-27 17:16:05 -0300

    Fix total

--- backend/src/main/java/com/devthiago/tbdelivery/entities/Order.java
+++ backend/src/main/java/com/devthiago/tbdelivery/entities/Order.java
@@ -33,6 +33,7 @@ public class Order implements Serializable {
 		joinColumns = @JoinColumn(name = "order_id"),
 		inverseJoinColumns = @JoinColumn(name = "product_id"))
 	private Set<Product> products = new HashSet<>();
+	
 
 	public Order() {
 
@@ -94,6 +95,14 @@ public class Order implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(Product p : products) {
+			sum += p.getPrice();
+		}
+		return sum;
 	}
 	
 
