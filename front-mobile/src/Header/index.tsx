1be6b0a5d402cb9af16d1bcba86665ae24c6e439
Thiago <thiagofreitas1106@gmail.com>
2021-01-27 23:59:07 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,23 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+    navigation.navigate('Home');
+  }
+
   return (
-    <View style={styles.container}>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
         <Image source={require('../assets/logo.png')} />
         <Text style={styles.text}>Thiago Delivery</Text>
-    </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-      backgroundColor: '#DA5C5C',
-      height: 90,
-      paddingTop: 50,
-      flexDirection: 'row',
-      justifyContent: 'center'
+    backgroundColor: '#DA5C5C',
+    height: 90,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center'
 
   },
   text: {
